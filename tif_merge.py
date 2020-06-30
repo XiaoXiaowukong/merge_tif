1f51cb217757952a718ccba084780915166dbe31
guoning <guoning@clustertech.com>
2020-06-30 15:55:27 +0800

    789

--- tif_merge.py
+++ tif_merge.py
@@ -17,27 +17,31 @@ def merge_tiles(input_pattern, output_path):
 
 
 def merge_tiles_size(tif_path, output_path, max_size):
-    merge_command = None
+    merge_tif = []
     for tif_index, tif_file in enumerate(os.listdir(tif_path)):
-        if tif_index == 0:
-            merge_command = ['gdal_merge.py', '-o',
-                             os.path.join(output_path, "{}_merged.tif".format(tif_index / max_size))]
-        else:
-            if tif_index % max_size == 0:
-                process(merge_command)
-                merge_command = ['gdal_merge.py', '-o',
-                                 os.path.join(output_path, "{}_merged.tif".format(tif_index / max_size))]
-            else:
-                merge_command.append(os.path.join(tif_path, tif_file))
-    if merge_command is not None:
-        process(merge_command)
+        if os.path.splitext(tif_file)[-1] != ".tif":
+            continue
+        merge_tif.append(os.path.join(tif_path, tif_file))
+    merge_count = merge_tif.__len__() / max_size
+    for split_index in range(merge_count + 1):
+        item_merge_tif = merge_tif[split_index * max_size:(split_index + 1) * max_size]
+        merge_command = ['gdal_merge.py', '-o',
+                         os.path.join(output_path, "{}_merged.tif".format(split_index))]
+        if item_merge_tif.__len__() == 0:
+            continue
+        merge_command.extend(item_merge_tif)
+        try:
+            subprocess.call(merge_command)
+        except Exception as e:
+            print (e.message)
 
 
-def process(command):
-    try:
-        subprocess.call(command)
-    except Exception as e:
-        print (e.message)
+def file_count(tif_root):
+    file_count = 0
+    for file_item in os.listdir(tif_root):
+        if os.path.isfile(os.path.join(tif_root, file_item)):
+            file_count += 1
+    return file_count
 
 
 if __name__ == '__main__':
@@ -48,4 +52,15 @@ if __name__ == '__main__':
     if not os.path.exists(tif_root) or not os.path.exists(merge_dir):
         print("dir is not exist")
         exit(1)
-    merge_tiles_size(tif_root, merge_dir, int(max_size))
+
+    all_count = file_count(tif_root)
+    level_dir = 0
+    while (all_count > 1):
+        print(level_dir)
+        merge_level_dir = os.path.join(merge_dir, str(level_dir))
+        if not os.path.exists(merge_level_dir):
+            os.mkdir(merge_level_dir)
+        merge_tiles_size(tif_root, merge_level_dir, int(max_size))
+        tif_root = merge_level_dir
+        all_count = file_count(merge_level_dir)
+        level_dir += 1
